9e7f7b0fb0421ff749d6c00bb47bdfc133e87570
joelmagner <joelfolkessonn@gmail.com>
2018-08-10 23:15:30 +0200

    Initial commit

---
+++ scam.py
@@ -0,0 +1,26 @@
+import requests
+import os
+import random
+import string
+import json
+
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+
+url = 'https://support-appleid-apple.service-legal-filedata.com/assets/includes/ProcessLogin.php'
+names = json.loads(open('names.json').read())
+
+for name in names:
+    name_extra = ''.join(random.choice(string.digits) for i in range(4))
+
+    username = name.lower() + name_extra + random.choice(name) + '@gmail.com'
+    password = ''.join(random.choice(chars) for i in range(9))
+
+    requests.post(url, allow_redirects=False, data={
+        'user': username,
+        'pass': password
+    })
+
+    print('sending', (username,password))
